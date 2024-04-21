3a2d937fefd8e0f17ecc7175577a8dd0443ec849
Taylor Broad <taylorbroad@gmail.com>
2024-04-22 00:03:03 +0900

    Fix Migrations for table checking and dropping

--- Database/migrations/2023_11_12_092039_create_trip_reports_table.php
+++ Database/migrations/2023_11_12_092039_create_trip_reports_table.php
@@ -16,6 +16,7 @@ class CreateTripReportsTable extends Migration
      */
     public function up()
     {
+        if (!Schema::hasTable('ch_trip_reports')) {
         Schema::create('ch_trip_reports', function (Blueprint $table) {
             $table->string('id');
             $table->foreignId('parent_id')->nullable();
@@ -26,6 +27,8 @@ class CreateTripReportsTable extends Migration
             $table->foreignId('aircraft_id')->nullable();
             $table->timestamps();
         });
+        }
+        if (!Schema::hasTable('ch_flight_pirep_trip')) {
         Schema::create('ch_flight_pirep_trip', function (Blueprint $table) {
             $table->id();
             $table->string('trip_report_id');
@@ -33,11 +36,14 @@ class CreateTripReportsTable extends Migration
             $table->string('pirep_id')->nullable();
             $table->integer('order');
         });
+        }
+        if (!Schema::hasTable('trip_report_user')) {
         Schema::create('trip_report_user', function (Blueprint $table) {
             $table->string('trip_report_id');
             $table->foreignId('user_id');
             $table->boolean('owner');
         });
+        }
     }
 
     /**
@@ -47,6 +53,8 @@ class CreateTripReportsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('trip_reports');
+        Schema::dropIfExists('ch_trip_reports');
+        Schema::dropIfExists('ch_flight_pirep_trip');
+        Schema::dropIfExists('trip_report_user');
     }
 }
