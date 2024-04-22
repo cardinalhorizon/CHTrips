15aea9304804e6ed1b3f3f033a64d898a2019e6e
Taylor Broad <taylorbroad@gmail.com>
2024-04-22 15:15:04 +0900

    Change Issue where PIREP Filing Interfered With Non-Trip Flights

--- Listeners/PirepAcceptedListener.php
+++ Listeners/PirepAcceptedListener.php
@@ -56,11 +56,14 @@ class PirepAcceptedListener extends Listener
 
         // Check for Trip Completion
 
-        $active_trip = FlightPirepTrip::with('trip_report')->where('pirep_id', $pirep_id)->first()->trip_report()->first();
-        if ($active_trip === null) {
+        $ftp_f = FlightPirepTrip::with("trip_report")
+            ->where("pirep_id", $pirep_id)
+            ->first();
+        if ($ftp_f === null) {
             return;
         }
-        $active_trip->load('fpts');
+        $active_trip = $ftp_f->trip_report()->first();
+        $active_trip->load("fpts");
         //dd($active_trip);
         $completed = true;
         foreach ($active_trip->fpts as $fpt) {
@@ -75,8 +78,10 @@ class PirepAcceptedListener extends Listener
             $active_trip->state = TripState::COMPLETED;
             $active_trip->save();
             // Delete the flights that are user created
-            Flight::whereHasMorph('owner', [TripReport::class], function (Builder $builder) use ($active_trip) {
-                $builder->where('owner_id', $active_trip->id);
+            Flight::whereHasMorph("owner", [TripReport::class], function (
+                Builder $builder
+            ) use ($active_trip) {
+                $builder->where("owner_id", $active_trip->id);
             })->delete();
 
             // Trigger the Trip Completion Event
